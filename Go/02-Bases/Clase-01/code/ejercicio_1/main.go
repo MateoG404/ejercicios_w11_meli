8a545c2158c7d6ffed9588e6defccfe67d18b01d
MateoG04 <mgutierrezca@unal.edu.co>
2023-12-28 11:04:28 -0500

    Solution first excercise Go bases

---
+++ Go/02-Bases/Clase-01/code/ejercicio_1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	name := "Mateo Gutierrez Melo"
+	direccion := "Bogota Colombia"
+
+	fmt.Println("Hola! Mi nombre es", name, "y vivo en ", direccion)
+}
