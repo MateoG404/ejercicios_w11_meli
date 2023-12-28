007c86d37ffab816474cdf15ab76d3eabf3ecf70
MateoG04 <mgutierrezca@unal.edu.co>
2023-12-28 11:12:44 -0500

    Solution third excercise Go bases

---
+++ Go/02-Bases/Clase-01/code/ejercicio_3/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Las correciones de las variables son :
+	var firstName, lastName string
+	var driverLicense = true
+	var personHeight, childsNumber, age int
+
+	lastName = "6"
+
+	childsNumber = 2
+
+	fmt.Println(firstName, lastName, driverLicense, age, personHeight, childsNumber)
+}
