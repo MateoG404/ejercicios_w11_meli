bf9f15b0944174846a17ebd090fede9ab02c069d
MateoG04 <mgutierrezca@unal.edu.co>
2024-01-02 08:52:14 -0500

    Solved excercise 1 class 03

---
+++ Go/02-Bases/Clase-03/code/ejercicio-1/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func impuestoSalario(salario float64) float64 {
+	if salario > 50000 {
+		return salario - (salario * 0.17)
+	} else if salario > 150000 {
+		return salario - (salario * 0.27)
+	}
+	return salario
+}
+
+func main() {
+
+	var salario float64
+	fmt.Println("Ingrese el salario de su empleado:")
+	fmt.Scan(&salario)
+
+	fmt.Println("El salario del empleado es:", impuestoSalario(salario))
+}
