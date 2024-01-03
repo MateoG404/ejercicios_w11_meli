7806ca9b9cca699b16a084121f946b719773dc53
MateoG04 <mgutierrezca@unal.edu.co>
2024-01-03 12:32:39 -0500

    Creation methods using the same name to use receptors

--- Go/02-Bases/Clase-04/notes/structs/app_structs/cmd/main.go
+++ Go/02-Bases/Clase-04/notes/structs/app_structs/cmd/main.go
@@ -10,14 +10,28 @@ func main() {
 	fmt.Println("Using structs")
 
 	// Create a new person
-	personA := internal.Person{FirstNmame: "Mateo", LastName: "Gutierrez", CellPhone: "123456789", Address: "Street 123"}
+	personA := internal.Person{FirstNmame: "Mateo", LastName: "Gutierrez", CellPhone: "123456789", Address: "Street 123", Password: "s"}
+	personB := internal.Person{FirstNmame: "Camila", LastName: "Zambrano", CellPhone: "123456789", Password: "s"}
 	fmt.Println(personA)
+	fmt.Println(personB)
 
-	// Store the person in a JSON file
+	// Store the personA in a JSON file
 	personJSON, err := json.Marshal(personA)
+
+	// Store the personB in a JSON file
+	personJSONB, err := json.Marshal(personB)
 	if err != nil {
 		fmt.Println("Error marshaling person:", err)
 		return
 	}
+
 	fmt.Println(string(personJSON))
+	fmt.Println(string(personJSONB))
+
+	// Create a new animal
+	animalA := internal.Animal{Name: "Mateo", Age: 22, Race: "Galgo ingles"}
+
+	// Print the name of the person and the animal
+	animalA.PrintName()
+	personA.PrintName()
 }
