2a434241ce6f26d0c4703220a86e4db249ac5143
MateoG04 <mgutierrezca@unal.edu.co>
2024-01-05 11:50:33 -0500

    Solved excercise 2 Class 06-Panic_&_Defer

--- Go/02-Bases/Clase-06/code/ejercicio-1/app_ejercicio1/cmd/main.go
+++ Go/02-Bases/Clase-06/code/ejercicio-1/app_ejercicio1/cmd/main.go
@@ -9,16 +9,19 @@ import (
 
 func main() {
 	fmt.Println("Inicio del programa para leer datos del estudio contable ")
-	// Defer if the file doesnt exist
-	defer fmt.Println("Ejecucion Finalizada")
+
 	// Read the file
 	file, err := os.Open("./customers.txt")
 
+	// Defer if the file doesnt exist
+	defer fmt.Println("Ejecucion Finalizada")
+
+	// Check if the file exists if not trough a panic
 	if err != nil {
-		panic(err)
+		panic("The indicated file was not found or is damaged")
 	}
 
-	// Read the file
+	// Read the file if the file exists
 	scanner := bufio.NewScanner(file)
 
 	// Create a variable to store the customers information
--- Go/02-Bases/Clase-06/code/ejercicio-2/app_ejercicio2/cmd/main.go
+++ Go/02-Bases/Clase-06/code/ejercicio-2/app_ejercicio2/cmd/main.go
@@ -1,5 +1,49 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func recoverPanic() {
+	if recover() != nil {
+		fmt.Println("RECOVERED: from the panic")
+	}
+}
 func main() {
 
+	fmt.Println("Inicio del programa para leer datos del estudio contable ")
+
+	// Read the file
+	file, err := os.Open("./cuddstomers.txt")
+
+	// Defer if the file doesnt exist
+	defer fmt.Println("Ejecucion Finalizada y cerrado del archivo")
+	defer file.Close()
+	defer recoverPanic()
+
+	// Check if the file exists if not trough a panic
+	if err != nil {
+		panic("The indicated file was not found or is damaged")
+
+	}
+
+	// Read the file if the file exists
+	scanner := bufio.NewScanner(file)
+
+	// Create a variable to store the customers information
+	var customers []string
+
+	// Read all the lines of the file
+
+	for scanner.Scan() {
+		customers = append(customers, scanner.Text())
+
+	}
+	// Show the customers information
+	fmt.Println("Customers: ")
+	fmt.Println(customers)
+	// Close the file
+	file.Close()
 }
