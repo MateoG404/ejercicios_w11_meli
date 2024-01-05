0d4804e3e8bf45ecf886b77f18af0b3b05aa1e9a
MateoG04 <mgutierrezca@unal.edu.co>
2024-01-05 08:01:43 -0500

    Append explanation solved excercise 1 Class 06-Errors

--- Go/02-Bases/Clase-05/code/ejercicio-3/app_ejercicio3/cmd/main.go
+++ Go/02-Bases/Clase-05/code/ejercicio-3/app_ejercicio3/cmd/main.go
@@ -14,14 +14,19 @@ func (e *Error) Error() string {
 }
 
 func main() {
+	// Declaration and initialization of variables
 	var salary int
 	fmt.Println("Ingrese su salario: ")
 
+	// Read the salary from the keyboard
 	fmt.Scan(&salary)
+
 	if salary < 15000 {
+		// Creation error
 		var err Error
 		fmt.Println(err.Error())
 	} else {
+		// Print message if the salary is greater than or equal to 15000
 		fmt.Println("Must pay tax")
 	}
 
