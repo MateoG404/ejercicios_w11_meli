7b4b062e8d2bf3d3696dc93ae2b149ab0a96b8b7
MateoG04 <mgutierrezca@unal.edu.co>
2024-01-09 12:52:24 -0500

    Creation example about how to implement API REST in a native way 👾

---
+++ Go/03-Web/Clase-01-API/notes/http/app/cmd/main.go
@@ -0,0 +1,48 @@
+// This scripts contains the way to create a server as a native way using the net/http package
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/fatih/color"
+)
+
+var (
+	green = color.New(color.FgGreen).SprintFunc()
+	blue  = color.New(color.FgBlue).SprintFunc()
+)
+
+func main() {
+	fmt.Println(green("Servidor iniciado"))
+
+	// Create the cases  for the handlerTest
+	handlerTest := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(blue("Saludos desde el endpoint prueba"))
+		// Escribe en el writer para retornarlo en el response
+		fmt.Fprintf(w, "Saludos desde el endpoint prueba")
+		// Start the server
+	}
+
+	handlerCases := func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			fmt.Fprintf(w, "Saludos desde el endpoint prueba usando GET")
+		case "POST":
+			fmt.Fprintf(w, "Saludos desde el endpoint prueba usando POST")
+
+		}
+	}
+	// Register the funcionalities for the handlerTest
+	http.HandleFunc("/prueba", handlerTest)
+
+	// Register a second handler
+	http.HandleFunc("/casos", handlerCases)
+	// Start the server
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
