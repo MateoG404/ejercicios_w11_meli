568a20fc82d47f2d4e195ad2b6cfd2dfdb99755e
MateoG04 <mgutierrezca@unal.edu.co>
2024-01-09 13:01:43 -0500

    Solved first task API golang - class 01 😎

--- Go/03-Web/Clase-01-API/code/ejercicio_1/app/cmd/main.go
+++ Go/03-Web/Clase-01-API/code/ejercicio_1/app/cmd/main.go
@@ -1,7 +1,10 @@
+// This script contains the solution for the first task of the class API
+// The task is to create a server using the net/http package with an endpoint that returns a text
 package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/fatih/color"
 )
@@ -11,8 +14,29 @@ var (
 )
 
 func main() {
-	fmt.Println(green("Inicio del programa"))
+	fmt.Println(green("Inicio del servidor"))
 
-	fmt.Println(green("Finalizacion del programa"))
+	// Define the endpoints in the handler
+	ping := func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			fmt.Fprintf(w, "pong")
+		default:
+			fmt.Fprintf(w, "Error 405: Method not allowed")
+
+		}
+	}
+
+	// Register the endpoints into the handler
+	http.HandleFunc("/ping", ping)
+
+	// Start the server
+
+	// listen and serve
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(green("Finalizacion del servidor"))
 
 }
