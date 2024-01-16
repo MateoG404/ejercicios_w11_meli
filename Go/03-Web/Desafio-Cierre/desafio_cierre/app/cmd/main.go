2f6164ec3a79479d1fe71b9c2662975e970976c6
MateoG04 <mgutierrezca@unal.edu.co>
2024-01-16 09:47:45 -0500

    Refactor to use environmental variables

--- Go/03-Web/Desafio-Cierre/desafio_cierre/app/cmd/main.go
+++ Go/03-Web/Desafio-Cierre/desafio_cierre/app/cmd/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"desafio_cierre/app/platform"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/fatih/color"
+	"github.com/joho/godotenv"
 )
 
 var (
@@ -13,16 +16,24 @@ var (
 )
 
 func main() {
+	// Use the environment variables
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Get the environment variables
+	addr := os.Getenv("ADDR")
+
 	// Start the application
 	fmt.Println(green("Inicio de la aplicación"))
 
 	// Configurate the environment variables
 	// Create and configurate the server
-	app := platform.NewServerChi(":8080")
+	app := platform.NewServerChi(addr)
 
 	// Start the server
 	if err := app.Start(); err != nil {
 		fmt.Println(red("Error al iniciar el servidor: %s\n", err.Error()))
 	}
-
 }
--- Go/03-Web/Desafio-Cierre/desafio_cierre/go.mod
+++ Go/03-Web/Desafio-Cierre/desafio_cierre/go.mod
@@ -2,9 +2,13 @@ module desafio_cierre
 
 go 1.21.5
 
-require github.com/fatih/color v1.16.0
+require (
+	github.com/fatih/color v1.16.0
+	github.com/go-chi/chi/v5 v5.0.11
+)
 
 require (
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	golang.org/x/sys v0.14.0 // indirect
