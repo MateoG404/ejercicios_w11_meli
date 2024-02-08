a590d034c2347ae9b45fc1507ecb92627654e676
MateoG404 <mgutierrezca@unal.edu.co>
2024-02-08 15:46:54 -0500

    feat: Solved task to implement Unit test in Shark and tunas

---
+++ Testing/code/ejercicio1/app/internal/white_shark_test.go
@@ -0,0 +1,93 @@
+package internal
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestWhiteSharl(t *testing.T) {
+	// Negative Test : shark is tired
+	t.Run("shark is tired", func(t *testing.T) {
+		// Arrange
+
+		// - Create a shark that is tired
+		shark := NewWhiteShark(true, true, 0)
+
+		// - Create a tuna that is fast
+		tuna := NewTuna("tuna", 1)
+
+		// Act
+		result := shark.Hunt(tuna)
+
+		// Assert
+		require.Equal(t, ErrSharkIsTired, result)
+	})
+
+	// Negative Test : shark already eaten a tuna
+	t.Run("shark hunts", func(t *testing.T) {
+		// Arrange
+		// - Create a shark that is hungry and no
+		shark := NewWhiteShark(false, false, 1)
+		// - Create a tuna that is slow
+		tuna := NewTuna("tuna", 1)
+
+		// Act
+		result := shark.Hunt(tuna)
+		// Assert
+		require.Nil(t, result)
+		require.Equal(t, ErrSharkIsNotHungry, result)
+	})
+
+	// Negative Test : the tuna is faster than the shark
+
+	t.Run("the tuna is faster than the shark", func(t *testing.T) {
+		// Arrange
+
+		// - Create a shark that is hungry and not tired
+		shark := NewWhiteShark(true, false, 1)
+		// - Create a tuna that is fast
+		tuna := NewTuna("tuna", 2)
+		// Act
+		result := shark.Hunt(tuna)
+
+		// Assert
+		require.Nil(t, result)
+		require.Equal(t, ErrSharkIsSlower, result)
+
+	})
+
+	// Positive Test : the shark hunts the tuna and the shark is not hungry anymore and is tired
+	t.Run("the shark hunts the tuna and the shark is not hungry anymore and is tired", func(t *testing.T) {
+		// Arrange
+
+		// - Create a shark that is hungry and not tired
+		shark := NewWhiteShark(true, false, 2)
+		// - Create a tuna that is slow
+		tuna := NewTuna("tuna", 1)
+		// Act
+		result := shark.Hunt(tuna)
+		// Assert
+		require.Nil(t, result) // no error
+
+		require.False(t, shark.Hungry) // shark is not hungry anymore
+		require.True(t, shark.Tired)   // shark is tired
+	})
+
+	// Unit Test : Tuna is nil
+	t.Run("tuna is nil", func(t *testing.T) {
+
+		// Arrange
+
+		// - Create a shark that is hungry and not tired
+		shark := NewWhiteShark(true, false, 2)
+		// - Create a tuna that is nil
+		var tuna *Tuna = nil
+		// Act
+		result := shark.Hunt(tuna)
+
+		// Assert
+		require.Equal(t, ErrTunaIsNil, result)
+	})
+
+}
---
+++ Testing/code/ejercicio1/go.mod
@@ -0,0 +1,3 @@
+module ejercicio_ejercicio1
+
+go 1.21.5
